ef8914a64da0bed6143a321469e188e64184ded0
yudai2929 <yudai201729@icloud.com>
2023-12-05 22:59:39 +0900

    ♻️refactor: コレクション関数に切り出し

---
+++ pkg/utils/filter.go
@@ -0,0 +1,11 @@
+package utils
+
+func Filter[T comparable](slice []T, f func(T) bool) []T {
+	result := make([]T, 0)
+	for _, s := range slice {
+		if f(s) {
+			result = append(result, s)
+		}
+	}
+	return result
+}
